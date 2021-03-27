cc47b32628ff66308e8e755ca6e3c85e0b940e01
Andrien <andrew6gog@gmail.com>
2021-03-27 12:23:18 +0400

    Еуые сщььше

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
+        bot.send_message(message.from_user.id, 'Прувет!')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
