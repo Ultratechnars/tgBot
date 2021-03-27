469099194f0bee8213d9507ababa117255336cb8
Andrien <andrew6gog@gmail.com>
2021-03-27 16:03:14 +0400

    Прувет to Привет

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Прувет!')
+        bot.send_message(message.from_user.id, 'Привет!')
         response = requests.get('https://kudago.com/public-api/v1.4/events/?lang=&fields=&expand=&order_by=&text_format=text&ids=&location=&actual_since=1444385206&actual_until=1444385405&is_free=&lon=&lat=&radius=').json()
         bot.send_message(message.from_user.id, response['results'][randint(0, 20)]['title'])
     elif message.text.lower() == 'ууууууу':
