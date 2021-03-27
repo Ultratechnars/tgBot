df3529080dab073adf50461e1d6bfe7c73334b0c
Paul <pakaich@mail.ru>
2021-03-27 14:22:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from telegramcalendar import *
 bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 
 @bot.message_handler(content_types=['text'])
-
 def get_text_messages(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Прувет!')
@@ -12,23 +11,16 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'УУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУ')
     elif message.text.lower() == '/start' or message.text.lower() == '/help':
         bot.reply_to(message, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
-    else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
-
-
-# Handles all sent documents and audio files
-@bot.message_handler(content_types=['document', 'audio'])
-def handle_docs_audio(message):
-	 bot.reply_to(message, 'ууууууууууууууууууууууууууууууууууууууууууууууууууууууууууаааааааааааааааааааааааааааааааааауууууууууууууууууууууууууууууууууууууааааааааааааааааааааааааааааааУУУУУУАААААААААААААААААААААААААААААААААААУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУАААААААААААААААААААААААААААААУАУААААААААААААААААААААААААААААААААААУАУАААААААААААААААААААААААААААААААААААААААААААААААААААААААААУАУАУАУАУУУУУУУУУУУУУУУУУУУУУУУУУУУУУААААААААААААААААААААААУУУУУУУУУУУ')
-
-@bot.message_handler(commands=['calendar'])
-def get_calendar(message):
-    now = datetime.datetime.now() #Текущая дата
+    elif message.text.lower() == '/calendar':
+        now = datetime.datetime.now() #Текущая дата
     chat_id = message.chat.id
     date = (now.year,now.month)
     current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
     markup = create_calendar(now.year,now.month)
     bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
     bot.answer_callback_query(call.id, text="Дата выбрана")
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
 
 bot.polling(none_stop=True)
