f377fe365cdf6262fa058cadb6ae2bebd60ebd57
Andrien <andrew6gog@gmail.com>
2021-03-27 15:08:07 +0400

    orks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 
 bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
@@ -9,7 +10,7 @@ def get_text_messages(message):
     elif message.text.lower() == 'ууууууу':
         bot.send_message(message.from_user.id, 'УУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУ')
     elif message.text.lower() == '/start' or message.text.lower() == '/help':
-        bot.reply_to(message, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
+        bot.send_message(message.from_user.id, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
