6a7dda8f68d0ea65f846db03526e59a2be8f2369
Paul <pakaich@mail.ru>
2021-03-27 15:01:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import telebot
-from telegramcalendar import *
 
 bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Прувет!')
-    elif message.text.lower() == 'ууууууу':
-        bot.send_message(message.from_user.id, 'УУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУ')
+        bot.send_message(message.from_user.id, 'Ghbdtn')
     elif message.text.lower() == '/start' or message.text.lower() == '/help':
-        bot.reply_to(message, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
-    elif message.text.lower() == '/calendar':
-        now = datetime.datetime.now() #Текущая дата
-        chat_id = message.chat.id
-        date = (now.year,now.month)
-        current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
-        markup = create_calendar(now.year,now.month)
-        bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
-        bot.answer_callback_query(call.id, text="Дата выбрана")
+        bot.reply_to(message, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')   
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
-
-
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
