7b47d5aa6c93df41ad1e7ea810bdaf05048af6c3
Paul <pakaich@mail.ru>
2021-03-27 14:13:55 +0300

    calendar added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telegramcalendar import *
 
 bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 
@@ -14,5 +15,19 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
 
+# Handles all sent documents and audio files
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_docs_audio(message):
+	 bot.reply_to(message, 'ууууууууууууууууууууууууууууууууууууууууууууууууууууууууууаааааааааааааааааааааааааааааааааауууууууууууууууууууууууууууууууууууууааааааааааааааааааааааааааааааУУУУУУАААААААААААААААААААААААААААААААААААУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУАААААААААААААААААААААААААААААУАУААААААААААААААААААААААААААААААААААУАУАААААААААААААААААААААААААААААААААААААААААААААААААААААААААУАУАУАУАУУУУУУУУУУУУУУУУУУУУУУУУУУУУУААААААААААААААААААААААУУУУУУУУУУУ')
+
+@bot.message_handler(commands=['calendar'])
+def get_calendar(message):
+    now = datetime.datetime.now() #Текущая дата
+    chat_id = message.chat.id
+    date = (now.year,now.month)
+    current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
+    markup = create_calendar(now.year,now.month)
+    bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
+    bot.answer_callback_query(call.id, text="Дата выбрана")
 
 bot.polling(none_stop=True)
