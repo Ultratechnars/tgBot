0344d893e61b46aba6c3c3225d31d8201fc9a263
Paul <pakaich@mail.ru>
2021-03-27 14:26:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def get_text_messages(message):
         bot.reply_to(message, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
     elif message.text.lower() == '/calendar':
         now = datetime.datetime.now() #Текущая дата
-    chat_id = message.chat.id
-    date = (now.year,now.month)
-    current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
-    markup = create_calendar(now.year,now.month)
-    bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
-    bot.answer_callback_query(call.id, text="Дата выбрана")
+        chat_id = message.chat.id
+        date = (now.year,now.month)
+        current_shown_dates[chat_id] = date #Сохраним текущую дату в словарь
+        markup = create_calendar(now.year,now.month)
+        bot.send_message(message.chat.id, "Пожалйста, выберите дату", reply_markup=markup)
+        bot.answer_callback_query(call.id, text="Дата выбрана")
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
