1f775f083e8d89f49261e877fdffce915f34ac13
Paul <pakaich@mail.ru>
2021-03-27 12:46:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 def get_text_messages(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Прувет!')
-    else if message.text.lower() == 'ууууууу':
+    elif message.text.lower() == 'ууууууу':
         bot.send_message(message.from_user.id, 'УУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУAAAAAAAAAAAAAAAAAAAAAAAAAAУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУ')
-    else if message.text.lower() == '/start' or message.text.lower() == '/help':
+    elif message.text.lower() == '/start' or message.text.lower() == '/help':
         bot.reply_to(message, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
