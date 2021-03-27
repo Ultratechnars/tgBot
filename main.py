61898a104c40daec10886a4accf73aa9daf553d6
Paul <pakaich@mail.ru>
2021-03-27 14:20:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ bot = telebot.TeleBot('1700380188:AAEUDoBpV9ATgEt-arqvYrdqcmwYi3MWmpc')
 
 
 @bot.message_handler(content_types=['text'])
+
 def get_text_messages(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Прувет!')
