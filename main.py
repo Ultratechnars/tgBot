fd87594bc3d9b22c84073c86c83d571554cba474
Paul <pakaich@mail.ru>
2021-03-27 12:34:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Прувет!')
     if message.text.lower() == 'ууууууу':
         bot.send_message(message.from_user.id, 'УУУУУУУУУУУУ\nУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУ')
-    elif message.text.lower() == '/start':
-        bot.send_message(message.from_user.id,
-                         'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, 'Этот бот будет присылать тебе крутую инфу про разные мероприятия ЛОЛ кринж кек!')
 
 bot.polling(none_stop=True)
